fea5cb110b3049851ec3856517e378b4f8f4becc
Saurabh <saurabh.gulia@ignite.world>
2017-08-04 15:12:17 +0530

    New stamp layout added

---
+++ SMOR/SMNewStampLayoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMNewStampLayoutViewController.h
+//  SMOR
+//
+//  Created by Saurabh on 8/4/17.
+//  Copyright © 2017 Gronical Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMNewStampLayoutViewController : UIViewController
+
+@end
---
+++ SMOR/SMNewStampLayoutViewController.m
@@ -0,0 +1,37 @@
+//
+//  SMNewStampLayoutViewController.m
+//  SMOR
+//
+//  Created by Saurabh on 8/4/17.
+//  Copyright © 2017 Gronical Studios. All rights reserved.
+//
+
+#import "SMNewStampLayoutViewController.h"
+
+@interface SMNewStampLayoutViewController ()
+
+@end
+
+@implementation SMNewStampLayoutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
